1d7b3759315a2b99354976ca4ee452c3c51fe32a
amirholya01 <120662275+amirhosseinolya@users.noreply.github.com>
2025-06-29 17:02:24 +0330

    Using Repository annotation

--- src/main/java/dk/dev/app/dao/impl/CustomerInMemoryDao.java
+++ src/main/java/dk/dev/app/dao/impl/CustomerInMemoryDao.java
@@ -2,13 +2,14 @@ package dk.dev.app.dao.impl;
 
 import dk.dev.app.dao.CustomerDAO;
 import dk.dev.app.model.Customer;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
 import java.util.Map;
 import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.atomic.AtomicLong;
-import java.util.stream.Collectors;
 
+@Repository
 public class CustomerInMemoryDao implements CustomerDAO {
 
 
