174adc3195cb699bfffc71687c82441208df8b45
amirholya01 <120662275+amirhosseinolya@users.noreply.github.com>
2025-06-29 16:57:46 +0330

    implementation CustomerDao

---
+++ src/main/java/dk/dev/app/dao/impl/CustomerInMemoryDao.java
@@ -0,0 +1,52 @@
+package dk.dev.app.dao.impl;
+
+import dk.dev.app.dao.CustomerDAO;
+import dk.dev.app.model.Customer;
+
+import java.util.List;
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.atomic.AtomicLong;
+import java.util.stream.Collectors;
+
+public class CustomerInMemoryDao implements CustomerDAO {
+
+
+    private final AtomicLong customerId = new AtomicLong(0);
+    private final Map<Long, Customer> customers = new ConcurrentHashMap<>();
+
+    @Override
+    public Customer save(Customer customer) {
+        Long id = customerId.incrementAndGet();
+        customer.setId(id);
+        customers.put(id, customer);
+        return customer;
+    }
+
+    @Override
+    public Customer update(Long id, Customer customer) {
+        customer.setId(id);
+        customers.put(id, customer);
+        return customer;
+    }
+
+    @Override
+    public boolean delete(Long id) {
+        return customers.remove(id) != null;
+    }
+
+    @Override
+    public Customer findById(Long id) {
+        return customers.get(id);
+    }
+
+    @Override
+    public List<Customer> findAll() {
+        return customers.values().stream().toList();
+    }
+
+    @Override
+    public boolean existsById(Long id) {
+        return customers.containsKey(id);
+    }
+}
