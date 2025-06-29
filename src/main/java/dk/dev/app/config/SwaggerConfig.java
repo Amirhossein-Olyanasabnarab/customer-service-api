5010a3cca94da04ed48e25d004d1deb7c851f119
amirholya01 <120662275+amirhosseinolya@users.noreply.github.com>
2025-06-29 16:41:11 +0330

    swagger configuration

---
+++ src/main/java/dk/dev/app/config/SwaggerConfig.java
@@ -0,0 +1,24 @@
+package dk.dev.app.config;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Contact;
+import io.swagger.v3.oas.models.info.Info;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SwaggerConfig {
+    @Bean
+    public OpenAPI customOpenAPI() {
+        return new OpenAPI()
+                .info(new Info()
+                        .title("Customer Service API")
+                        .description("Customer Service API")
+                        .version("1.0.0")
+                        .contact(new Contact()
+                                .name("Amir Hossein Olyanasab")
+                                .email("amirholya01@gmail.com")
+                        )
+                );
+    }
+}
