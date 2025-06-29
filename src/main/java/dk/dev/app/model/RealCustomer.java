6c0c1abe72b46ee777f277f2ff1196c633ad22b7
amirholya01 <120662275+amirhosseinolya@users.noreply.github.com>
2025-06-29 16:28:46 +0330

    creating real and legal customers

---
+++ src/main/java/dk/dev/app/model/RealCustomer.java
@@ -0,0 +1,21 @@
+package dk.dev.app.model;
+
+import dk.dev.app.enums.CustomerType;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.experimental.SuperBuilder;
+
+@Getter
+@Setter
+@SuperBuilder
+@AllArgsConstructor
+public class RealCustomer extends Customer {
+
+    private String nationality;
+    private String age;
+
+    public RealCustomer(){
+        this.setType(CustomerType.REAL);
+    }
+}
