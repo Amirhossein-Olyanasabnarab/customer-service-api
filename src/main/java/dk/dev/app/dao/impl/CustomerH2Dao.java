82e6b7f93eab1092173e92b9f7ac2b50873578b7
amirholya01 <120662275+amirhosseinolya@users.noreply.github.com>
2025-06-30 18:13:57 +0330

    save customer with h2Dao

--- src/main/java/dk/dev/app/dao/impl/CustomerH2Dao.java
+++ src/main/java/dk/dev/app/dao/impl/CustomerH2Dao.java
@@ -2,13 +2,20 @@ package dk.dev.app.dao.impl;
 
 import dk.dev.app.dao.CustomerDAO;
 import dk.dev.app.model.Customer;
+import dk.dev.app.model.LegalCustomer;
+import dk.dev.app.model.RealCustomer;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Primary;
 import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.support.GeneratedKeyHolder;
+import org.springframework.jdbc.support.KeyHolder;
 import org.springframework.stereotype.Repository;
 
+import java.sql.PreparedStatement;
 import java.util.List;
 
 @Repository
+@Primary
 public class CustomerH2Dao implements CustomerDAO {
 
 
@@ -20,7 +27,29 @@ public class CustomerH2Dao implements CustomerDAO {
 
     @Override
     public Customer save(Customer customer) {
-        return null;
+        String customerSql = "INSERT INTO customer (full_name, phone_number, email, type) VALUES (?, ?, ?, ?)";
+        KeyHolder keyHolder = new GeneratedKeyHolder();
+        jdbc.update(connection -> {
+            PreparedStatement ps = connection.prepareStatement(customerSql, new String[]{"id"});
+            ps.setString(1, customer.getFullName());
+            ps.setString(2, customer.getPhoneNumber());
+            ps.setString(3, customer.getEmail());
+            ps.setString(4, customer.getType().name());
+            return ps;
+        }, keyHolder);
+
+        Long id = keyHolder.getKey().longValue();
+        customer.setId(id);
+
+        if (customer instanceof RealCustomer realCustomer) {
+            String realCustomerSql = "INSERT INTO real_customer (id, nationality, age) VALUES (?, ?, ?)";
+            jdbc.update(realCustomerSql, id, realCustomer.getNationality(), realCustomer.getAge());
+        } else if (customer instanceof LegalCustomer legalCustomer) {
+            String legalCustomerSql = "INSERT INTO legal_customer (id, company_name, industry) VALUES (?, ?, ?)";
+            jdbc.update(legalCustomerSql, id, legalCustomer.getCompanyName(), legalCustomer.getIndustry());
+        }
+
+        return customer;
     }
 
     @Override
