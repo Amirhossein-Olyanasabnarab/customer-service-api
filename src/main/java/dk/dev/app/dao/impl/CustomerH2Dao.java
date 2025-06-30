cb4ec768b8dc635cca0d4b96c613f7caf51a1749
amirholya01 <120662275+amirhosseinolya@users.noreply.github.com>
2025-06-30 17:51:03 +0330

    creating schema.sql

--- src/main/java/dk/dev/app/dao/impl/CustomerH2Dao.java
+++ src/main/java/dk/dev/app/dao/impl/CustomerH2Dao.java
@@ -2,12 +2,22 @@ package dk.dev.app.dao.impl;
 
 import dk.dev.app.dao.CustomerDAO;
 import dk.dev.app.model.Customer;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
 @Repository
 public class CustomerH2Dao implements CustomerDAO {
+
+
+    private final JdbcTemplate jdbc;
+    @Autowired
+    public CustomerH2Dao(JdbcTemplate jdbc) {
+        this.jdbc = jdbc;
+    }
+
     @Override
     public Customer save(Customer customer) {
         return null;
---
+++ src/main/resources/schema.sql
@@ -0,0 +1,21 @@
+create table customer(
+    id BIGINT auto_icrement primary key,
+    full_name varchar(100) not null ,
+    phone_number varchar(20) not null ,
+    email varchar(50),
+    type varchar(10) not null
+);
+
+create table real_customer(
+    id bigint primary key ,
+    nationality varchar(20) not null ,
+    age varchar(5) not null ,
+    foreign key (id) references customer(id) on delete cascade
+);
+
+create table legal_customer(
+    id bigint primary key ,
+    company_name varchar(20) not null ,
+    industry varchar(20),
+    foreign key (id) references customer(id) on delete cascade
+);
