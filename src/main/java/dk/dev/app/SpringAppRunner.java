15d77c5521886e8dc5421bcbd833ff7fd00eaba6
amirholya01 <120662275+amirhosseinolya@users.noreply.github.com>
2025-07-02 10:28:28 +0330

    Renaming main class

--- src/main/java/dk/dev/app/SpringAppRunner.java
+++ src/main/java/dk/dev/app/SpringAppRunner.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class CustomeServiceApiApplication {
+public class SpringAppRunner {
 
     public static void main(String[] args) {
-        SpringApplication.run(CustomeServiceApiApplication.class, args);
+        SpringApplication.run(SpringAppRunner.class, args);
     }
 
 }
